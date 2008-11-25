33cb1ff539c15f8ca1458cb320f2921e10b1ddd6
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-11-24 16:34:10 -0800

    some utility code that should be considered beta

---
+++ lib/cache/local.rb
@@ -0,0 +1,59 @@
+module Cache
+  class Local
+    delegate :respond_to?, :to => :@remote_cache
+        
+    def initialize(remote_cache)
+      @remote_cache = remote_cache
+    end
+ 
+    def cache_locally
+      @remote_cache = LocalCacheBuffer.new(original_cache = @remote_cache)
+      yield
+    ensure
+      @remote_cache = original_cache
+    end
+    
+    def method_missing(method, *args, &block)
+      @remote_cache.send(method, *args, &block)
+    end
+  end
+  
+  class LocalBuffer
+    delegate :respond_to?, :to => :@remote_cache
+    
+    def initialize(remote_cache)
+      @local_cache = {}
+      @remote_cache = remote_cache
+    end
+  
+    def get(key, *options)
+      if @local_cache.has_key?(key)
+        @local_cache[key]
+      else
+        @local_cache[key] = @remote_cache.get(key, *options)
+      end
+    end
+    
+    def set(key, value, *options)
+      @remote_cache.set(key, value, *options)
+      @local_cache[key] = value
+    end
+    
+    def add(key, value, *options)
+      result = @remote_cache.add(key, value, *options)
+      if result == "STORED\r\n"
+        @local_cache[key] = value
+      end
+      result
+    end
+    
+    def delete(key, *options)
+      @remote_cache.delete(key, *options)
+      @local_cache.delete(key)
+    end
+    
+    def method_missing(method, *args, &block)
+      @remote_cache.send(method, *args, &block)
+    end
+  end
+end
---
+++ lib/cache/mock.rb
@@ -0,0 +1,82 @@
+module Cache
+  class Mock < HashWithIndifferentAccess
+    attr_accessor :servers
+
+    def get_multi(*values)
+      reject { |k,v| !values.include? k }
+    end
+
+    def []=(key, value)
+      super(key, deep_clone(value))
+    end
+
+    def [](key)
+      deep_clone(super(key))
+    end
+
+    def set(key, value, *options)
+      self[key] = value
+    end
+
+    def get(key, *args)
+      self[key]
+    end
+
+    def incr(key, amount = 1)
+      self[key] ||= 0
+      self[key] += amount
+    end
+
+    def decr(key, amount = 1)
+      self[key] ||= 0
+      self[key] -= amount
+    end
+
+    def add(key, value, *options)
+      if self[key]
+        "NOT_STORED\r\n"
+      else
+        self[key] = value
+        "STORED\r\n"
+      end
+    end
+
+    def delete(key, *options)
+      self[key] = nil
+    end
+
+    def append(key, value)
+      set(key, get(key).to_s + value.to_s)
+    end
+
+    def namespace
+      nil
+    end
+
+    def flush_all
+      clear
+    end
+
+    def stats
+      {}
+    end
+
+    def reset_runtime
+      [0, Hash.new(0)]
+    end
+
+    private
+
+    def marshal(obj)
+      Marshal.dump(obj)
+    end
+
+    def unmarshal(marshaled_obj)
+      Marshal.load(marshaled_obj)
+    end
+
+    def deep_clone(obj)
+      unmarshal(marshal(obj))
+    end
+  end
+end
